cd290a944de02c8b2b1496e18ee53cd84a409abd
Diego <diego@Diegos-Laptop.local>
2023-06-13 18:21:49 -0500

    changes: tab 2 updated

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/interfaces';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
   categories: string[] = ["business","entertainment","general","health","science","sports","technology"]
   selectedCategory:string=this.categories[0];
+  articles: Article[] = [];
 
-  constructor() {}
+
+
+  constructor(
+    private newService: NewsService
+  ) {}
+  ngOnInit(): void {
+    this.newService.getTopHeadlineByCategory(this.selectedCategory)
+    .subscribe(article => {
+      this.articles = article
+    })
+  }
 
   segmentChanged(category: any) {
-    console.log(category)
+   this.selectedCategory = category.detail.value;
+    console.log(category.detail.value)
   }
 
 }
--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -4,20 +4,26 @@ import { environment } from 'src/environments/environment';
 import { Article, News } from '../interfaces';
 import { Observable, map } from 'rxjs';
 
-
 const apiKey = environment.apiKey;
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class NewsService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTopHeadlines(): Observable<Article[]> {
-    return this.http.get<News>(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`).pipe(
-      map(({articles}) => articles)
-    );
+    return this.http
+      .get<News>(
+        `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`
+      )
+      .pipe(map(({ articles }) => articles));
   }
 
+  getTopHeadlineByCategory(category: string): Observable<Article[]> {
+    return this.http
+      .get<News>(
+        `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`
+      )
+      .pipe(map(({ articles }) => articles));
+  }
 }
